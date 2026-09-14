efe7216b7f7003e486ac8fa8d92834a06fa65954
Boppana Naga Venkata Mahidhar Babu <fswd2004@gmail.com>
2026-09-14 18:51:32 +0530

    fix(cors): remove invalid options wildcard

--- server/src/app.js
+++ server/src/app.js
@@ -18,14 +18,6 @@ app.use(
   }),
 );
 
-app.options(
-  "*",
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  }),
-);
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
